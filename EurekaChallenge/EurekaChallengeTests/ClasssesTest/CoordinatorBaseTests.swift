f3e065145173d093efc186dbb527e744f536f9d8
luchoOlivera <oliveralucianof@gmail.com>
2022-10-06 10:10:46 -0300

    more test for coordinator

--- EurekaChallenge/EurekaChallengeTests/ClasssesTest/CoordinatorBaseTests.swift
+++ EurekaChallenge/EurekaChallengeTests/ClasssesTest/CoordinatorBaseTests.swift
@@ -11,39 +11,39 @@ import XCTest
 
 class CoordinatorBaseTests: XCTestCase, TestBase {
   var sut: CoordinatorBase!
-  var coordinatorBase: GenericCoordinatorBase!
+  var genericCoordinatorBase: GenericCoordinatorBase!
+    
+  override func tearDown() {
+    self.sut = nil
+    self.genericCoordinatorBase = nil
+    super.tearDown()
+  }
   
   override func setUp() {
     super.setUp()
     self.sut = CoordinatorBase()
-    self.coordinatorBase = GenericCoordinatorBase(rootViewController: UIViewController(), navVC: UINavigationController())
+    self.genericCoordinatorBase = GenericCoordinatorBase(rootViewController: UIViewController(), navVC: UINavigationController())
   }
   
-  override func tearDown() {
-    self.sut = nil
-    self.coordinatorBase = nil
-    super.tearDown()
-  }
-  
-  func testPushCoordinatorSetsTheCorrectParent() {
-    self.sut.pushCoordinator(coordinator: &coordinatorBase,
-                             rootVC: coordinatorBase.rootViewController, animated: false)
+  func testPushCoordinatorSetsTheCorrect() {
+    self.sut.pushCoordinator(coordinator: &genericCoordinatorBase,
+                             rootVC: genericCoordinatorBase.rootViewController, animated: false)
     
     XCTAssertEqual(sut.childCoordinators.count, 1)
-    XCTAssertEqual(sut.childCoordinators.first, coordinatorBase)
+    XCTAssertEqual(sut.childCoordinators.first, genericCoordinatorBase)
     XCTAssertEqual(sut.childCoordinators.first!.parentCordinator, sut)
   }
   
-  func testPushCoordinatorSetsTheCorrectNavigationFlow() {
-    self.sut.pushCoordinator(coordinator: &coordinatorBase,
-                             rootVC: coordinatorBase.rootViewController, animated: false)
-    self.coordinatorBase.navVC.pushViewController(UIViewController(), animated: false)
+  func testPushCoordinatorSetsFlow() {
+    self.sut.pushCoordinator(coordinator: &genericCoordinatorBase,
+                             rootVC: genericCoordinatorBase.rootViewController, animated: false)
+    self.genericCoordinatorBase.navVC.pushViewController(UIViewController(), animated: false)
     
     XCTAssertEqual(sut.childCoordinators.first!.navVC.children.count, 2)
-    XCTAssertEqual(sut.childCoordinators.first!.navVC.children.first, coordinatorBase.rootViewController)
+    XCTAssertEqual(sut.childCoordinators.first!.navVC.children.first, genericCoordinatorBase.rootViewController)
   }
   
-  func testPopCoordinatorRemovesTheCorrectCoordinator() {
+  func testPopCoordinatorRemoves() {
     self.givenACoordinatorBaseWithTwoChildCoordinator()
     
     let lastChild = self.sut.childCoordinators.last
@@ -53,8 +53,8 @@ class CoordinatorBaseTests: XCTestCase, TestBase {
     XCTAssertFalse(sut.childCoordinators.first === lastChild)
   }
   
-  func testPopCoordinatorRemovesAllViewConctrollersContainedInAChild() {
-    self.givenACoordinatorBaseWithTwoChildCoordinatorsWithTwoViewControllers()
+  func testPopCoordinatorRemovesAllViewInAChild() {
+    self.givenACoordinatorBaseWithTwoChildCoordinators()
     
     let child = self.sut.childCoordinators.last
     self.sut.popCoordinator(coordinator: child!, animated: false)
@@ -64,22 +64,22 @@ class CoordinatorBaseTests: XCTestCase, TestBase {
   }
   
   func givenACoordinatorBaseWithTwoChildCoordinator() {
-    var secondCoordinatorBase = GenericCoordinatorBase(rootViewController: UIViewController(), navVC: self.coordinatorBase.navVC)
-    self.sut.pushCoordinator(coordinator: &coordinatorBase,
-                             rootVC: coordinatorBase.rootViewController,
+    var secondCoordinatorBase = GenericCoordinatorBase(rootViewController: UIViewController(), navVC: self.genericCoordinatorBase.navVC)
+    self.sut.pushCoordinator(coordinator: &genericCoordinatorBase,
+                             rootVC: genericCoordinatorBase.rootViewController,
                              animated: false)
     self.sut.pushCoordinator(coordinator: &secondCoordinatorBase,
                              rootVC: secondCoordinatorBase.rootViewController,
                              animated: false)
   }
   
-  func givenACoordinatorBaseWithTwoChildCoordinatorsWithTwoViewControllers() {
-    self.sut.pushCoordinator(coordinator: &coordinatorBase,
-                             rootVC: coordinatorBase.rootViewController,
+  func givenACoordinatorBaseWithTwoChildCoordinators() {
+    self.sut.pushCoordinator(coordinator: &genericCoordinatorBase,
+                             rootVC: genericCoordinatorBase.rootViewController,
                              animated: false)
-    self.coordinatorBase.navVC.pushViewController(UIViewController(), animated: false)
+    self.genericCoordinatorBase.navVC.pushViewController(UIViewController(), animated: false)
     
-    var secondCoordinatorBase = GenericCoordinatorBase(rootViewController: UIViewController(), navVC: self.coordinatorBase.navVC)
+    var secondCoordinatorBase = GenericCoordinatorBase(rootViewController: UIViewController(), navVC: self.genericCoordinatorBase.navVC)
     
     self.sut.pushCoordinator(coordinator: &secondCoordinatorBase,
                              rootVC: secondCoordinatorBase.rootViewController,
--- EurekaChallenge/EurekaChallengeTests/ProtocolsTests/TestBase.swift
+++ EurekaChallenge/EurekaChallengeTests/ProtocolsTests/TestBase.swift
@@ -11,5 +11,6 @@ protocol TestBase {
   associatedtype SystemUnderTest
     /// The system under test(AKA: sut) represents
     /// the object intended to test.
+    /// reference : https://www.netguru.com/blog/testing-swift-ios-navigation
   var sut: SystemUnderTest! { get set }
 }
